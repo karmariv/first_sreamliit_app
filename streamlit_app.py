b3f91af41cd905e8a5e5fda92024500b9c40c837
karmariv <karmariv82@gmail.com>
2023-09-12 21:41:45 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,15 @@ streamlit.header('Breakfast Manu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+
+streamlit.header('Fruityvice Fruit Advice!')
+fruityvice_responce = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_responce.json())
+
+#take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+#output it the screen as a table
+streamlit.dataframe(fruityvice_normalized)
