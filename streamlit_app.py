94a468856681d55e8b6545b37be7a52aad5a720b
karmariv <karmariv82@gmail.com>
2023-09-13 20:29:19 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,4 @@ my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
- return fruityvice_normalized
+  return fruityvice_normalized
